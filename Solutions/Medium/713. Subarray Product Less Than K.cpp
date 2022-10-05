924694bc2801c105472d5cf28444117a31cee8e7
shady hossam <32609128+ShadyHosam@users.noreply.github.com>
2022-10-05 20:13:32 +0200

    713. Subarray Product Less Than K

---
+++ Solutions/Medium/713. Subarray Product Less Than K.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+   int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if(k <= 1) return 0;
+        int prod = 1 , res = 0, left = 0;
+        for(int right = 0; right < nums.size(); right++) {
+            prod *= nums[right];
+            while(prod >= k) {
+                prod /= nums[left];
+                left++;
+            }
+            res += right - left + 1;
+        }
+        return res;
+    }
+};
