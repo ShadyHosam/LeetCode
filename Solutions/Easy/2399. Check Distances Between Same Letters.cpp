188f719362269b852d2c7c582da627a7dbf0703a
shady hossam <32609128+ShadyHosam@users.noreply.github.com>
2022-10-05 20:49:48 +0200

    2399. Check Distances Between Same Letters

--- Solutions/Easy/2399. Check Distances Between Same Letters.cpp
+++ Solutions/Easy/2399. Check Distances Between Same Letters.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    bool checkDistances(string s, vector<int>& distance) {
+        
+        int n = s.size();
+        
+        for (int i = 0 ; i <  n ; i++)
+        {
+            for (int j = i +1 ; j <n ;j++)
+            {
+                if (s[i]==s[j])
+                {
+                    int dist = j - i - 1;
+                    if (distance[s[i]-'a']!=dist)return false;
+                }
+            }
+            
+        }
+        return true;
+        
+    }
+};
